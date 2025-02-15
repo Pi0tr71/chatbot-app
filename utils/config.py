8c0100da9c763660ccadc9bd7d7e0505428e2752
Piotr <92324841+Pi0tr71@users.noreply.github.com>
2025-02-15 19:35:56 +0100

    add image to rep

--- utils/config.py
+++ utils/config.py
@@ -13,4 +13,4 @@ def load_config():
     if os.path.exists(CONFIG_PATH):
         with open(CONFIG_PATH, "r") as f:
             return json.load(f)
-    return {"api_keys": {"openai": "", "nebius": "", "sambanova": "" }, "model": "gpt-4o"}
+    return {"api_keys": {"openai": "", "nebius": "", "sambanova": "" }, "model": "gpt-4o-mini"}
